ccbcef8d6fdf47f11ab17ec2f3efaa7cddd6d139
Sebastian Kurscheid <genomicsio@gmail.com>
2016-01-20 12:34:13 +1100

    SGE script to index CanFam3 with gem-indexer

---
+++ SGE_scripts/gem-index.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -V
+
+export data_dir=~/Data/RefGenomes/Canis_familiaris/Ensembl
+
+gem-indexer -i ${data_dir}/Canis_familiaris.CanFam3.1.dna_rm.toplevel.fa \
+            -o CanFam3.1 \
+            
