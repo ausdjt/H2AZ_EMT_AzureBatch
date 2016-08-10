06f52ea8156265365a04515970b4937ca542f4ff
Sebastian Kurscheid <genomicsio@gmail.com>
2016-08-10 16:40:02 +1000

    adds rule to run kallisto un de-compressed FASTQ files

--- snakemake/rules/run_kallisto.py
+++ snakemake/rules/run_kallisto.py
@@ -6,6 +6,15 @@ from snakemake.exceptions import MissingInputException
 
 wrapper_dir = "/home/skurscheid/Development/snakemake-wrappers/bio"
 
+rule run_kallisto_uncompressed:
+    input:
+        expand("./{assayID}/{runID}/{outdir}/{reference_version}/uncompressed/kallisto/{unit}",
+               runID = "NB501086_0067_RDomaschenz_JCSMR_RNASeq",
+               assayID = "RNA-Seq",
+               outdir = config["processed_dir"],
+               reference_version = config["references"]["version"],
+               unit = config["RNA-Seq"])
+
 rule kallisto_quant:
     message:
         "Running kallisto quant..."
@@ -18,6 +27,27 @@ rule kallisto_quant:
         ki = lambda wildcards: config["references"]["kallisto"][wildcards.reference_version]
     output:
         "./{assayID}/{runID}/{processed_dir}/{reference_version}/kallisto/{unit}"
+    shell:
+        """
+            kallisto quant --index={input.ki} \
+                           --output-dir={output} \
+                           --threads=4 \
+                           --bootstrap-samples={params.bootstraps} \
+                           {input[0]} {input[1]}
+        """
+
+rule kallisto_quant_from_uncompressed:
+    message:
+        "Running kallisto quant with uncompressed FASTQ as input..."
+    params:
+        bootstraps = config["kallisto"]["bootstraps"],
+        threads = 4
+    input:
+        read1 = lambda wildcards: "./" + wildcards.assayID + "/" + wildcards.runID + "/fastq/" + config[wildcards.assayID][wildcards.unit][0].replace(".gz", ""),
+        read2 = lambda wildcards: "./" + wildcards.assayID + "/" + wildcards.runID + "/fastq/" + config[wildcards.assayID][wildcards.unit][1].replace(".gz", ""),
+        ki = lambda wildcards: config["references"]["kallisto"][wildcards.reference_version]
+    output:
+        "./{assayID}/{runID}/{processed_dir}/{reference_version}/uncompressed/kallisto/{unit}"
     shell:
         """
             kallisto quant --index={input.ki} \
