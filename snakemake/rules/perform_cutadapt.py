7ffcb2a0f0e745497f26a3d84461a1a50df0aeed
Sebastian Kurscheid <genomicsio@gmail.com>
2016-11-23 11:52:42 +1100

    bug fix

--- snakemake/rules/perform_cutadapt.py
+++ snakemake/rules/perform_cutadapt.py
@@ -19,7 +19,7 @@ For usage, include this in your workflow.
 rule cutadapt_pe:
     """Trims given paired-end reads with given parameters"""
     params:
-        trim_params = config["program_parameters"]["trim_params"],
+        trim_params = config["program_parameters"]["cutadapt"]["trim_params"],
         trim_data = config["trim_dir"],
         raw_data = config["raw_dir"],
         cutadapt_dir = config["cutadapt_dir"]
