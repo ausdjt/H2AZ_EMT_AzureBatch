60df622ac7e00760b4fe5314d8f293ba3ed74850
Sebastian Kurscheid <genomicsio@gmail.com>
2016-11-29 15:54:20 +1100

    makes changes to display DataTables and volcano plots

--- shiny/MDCK_EMT_RNA-Seq/server.R
+++ shiny/MDCK_EMT_RNA-Seq/server.R
@@ -1,5 +1,5 @@
-#
 # This is the server logic of a Shiny web application. You can run the 
+#
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
@@ -8,8 +8,121 @@
 #
 
 library(shiny)
+library(DT)
 
-# Define server logic required to draw a histogram
+# Data loading and preparation
+load("~/Data/Tremethick/EMT/RNA-Seq/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/processed_data/CanFam3.1_ensembl84_ERCC/R_Analysis/resultsCompressed.rda", .GlobalEnv)
+load("~/Data/Tremethick/EMT/RNA-Seq/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/processed_data/CanFam3.1_ensembl84_ERCC/R_Analysis/cfam.qPCRGenesTab.rda", .GlobalEnv)
+load("~/Data/Tremethick/EMT/RNA-Seq/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/processed_data/CanFam3.1_ensembl84_ERCC/R_Analysis/ensGenes_CanFam3.1_ensembl84_ERCC.rda", .GlobalEnv)
+
+globalData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), function(y){
+  s <- resultsCompressed[[1]]$sleuth_results.gene[[y]]$target_id %in% ensGenes$ensembl_gene_id
+  dat <- resultsCompressed[[1]]$sleuth_results.gene[[y]][s,]
+  dat <- merge(dat,
+               ensGenes[,c("ensembl_gene_id", "external_gene_name", "description")], 
+               by.x = "target_id", 
+               by.y = "ensembl_gene_id")
+  dat <- dat[order(dat$qval), ]
+  return(list(dataTable = dat))
+})
+names(globalData) <- names(resultsCompressed[[1]]$sleuth_results.gene)
+
+emtMarkerData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), function(y){
+  s <- resultsCompressed[[1]]$sleuth_results.gene[[y]]$target_id %in% cfam.qPCRGenesTab$ensembl_gene_id
+  dat <- resultsCompressed[[1]]$sleuth_results.gene[[y]][s,]
+  dat <- merge(dat,
+               ensGenes[,c("ensembl_gene_id", "external_gene_name", "description")], 
+               by.x = "target_id", 
+               by.y = "ensembl_gene_id")
+  dat <- dat[order(dat$qval), ]
+  return(list(dataTable = dat))
+})
+names(emtMarkerData) <- names(resultsCompressed[[1]]$sleuth_results.gene)
+  
+# Actual Server logic
 shinyServer(function(input, output) {
-   
+  output$volcano_tgfb <- renderPlot({
+    y <- "conditionMDCKTGFb"
+    dat <- emtMarkerData[[y]]$dataTable
+    xAxisMax <- max(abs(dat$b)) + 1
+    plot(dat$b,
+         -log10(dat$qval), 
+         axes = F, 
+         xlab = "", 
+         ylab = "", 
+         frame = F,
+         cex = 0.3,
+         xlim = c(-round(xAxisMax, 0), round(xAxisMax,0)),
+         pch = 16, main = paste("Volcano plot\nCondition: ", y, sep = ""))
+    points(dat[which(-log10(dat$qval) >= 1), "b"], 
+           -log10(dat[which(-log10(dat$qval) >= 1), "qval"]),
+           col = "red", 
+           pch = 16, 
+           cex = 1.1)
+    axis(2,
+         pos = 0, 
+         lwd = 3)
+    #                             at = c(seq(0,yAxisMax,10)))
+    axis(1,
+         pos = 0, 
+         lwd = 3,
+         at = c(seq(-round(xAxisMax, 0), round(xAxisMax,0), 2)))
+    mtext("-log10(q-value)", 
+          side = 2)
+    mtext("beta value",
+          side = 1, 
+          line = 2)
+    abline(h = 1, col = "red", lty = 2, lwd = 2)
+    # only adding labels to genes with adjuste p-value <= 0.01
+    text(dat[which(-log10(dat$qval) >= 2), "b"], -log10(dat[which(-log10(dat$qval) >= 2), "qval"]), 
+         labels = dat[which(-log10(dat$qval) >= 2), "external_gene_name" ],
+         cex = 0.7,
+         pos = 4, offset = 0.3)
+    text(-4,1.2, "< 0.1 [adjusted p-value]", cex = 0.7)
+  })
+  
+  output$volcano_shZ <- renderPlot({
+    y <- "conditionMDCKshZ"
+    dat <- emtMarkerData[[y]]$dataTable
+    xAxisMax <- max(abs(dat$b)) + 1
+    plot(dat$b,
+         -log10(dat$qval), 
+         axes = F, 
+         xlab = "", 
+         ylab = "", 
+         frame = F,
+         cex = 0.3,
+         xlim = c(-round(xAxisMax, 0), round(xAxisMax,0)),
+         pch = 16, main = paste("Volcano plot\nCondition: ", y, sep = ""))
+    points(dat[which(-log10(dat$qval) >= 1), "b"], 
+           -log10(dat[which(-log10(dat$qval) >= 1), "qval"]),
+           col = "red", 
+           pch = 16, 
+           cex = 1.1)
+    axis(2,
+         pos = 0, 
+         lwd = 3)
+    #                             at = c(seq(0,yAxisMax,10)))
+    axis(1,
+         pos = 0, 
+         lwd = 3,
+         at = c(seq(-round(xAxisMax, 0), round(xAxisMax,0), 2)))
+    mtext("-log10(q-value)", 
+          side = 2)
+    mtext("beta value",
+          side = 1, 
+          line = 2)
+    abline(h = 1, col = "red", lty = 2, lwd = 2)
+    # only adding labels to genes with adjuste p-value <= 0.01
+    text(dat[which(-log10(dat$qval) >= 2), "b"], -log10(dat[which(-log10(dat$qval) >= 2), "qval"]), 
+         labels = dat[which(-log10(dat$qval) >= 2), "external_gene_name" ],
+         cex = 0.7,
+         pos = 4, offset = 0.3)
+    text(-4,1.2, "< 0.1 [adjusted p-value]", cex = 0.7)
+  })
+  
+  output$global_de_tgfb = DT::renderDataTable(globalData[["conditionMDCKTGFb"]]$dataTable)
+  output$global_de_shZ = DT::renderDataTable(globalData[["conditionMDCKshZ"]]$dataTable)
+  output$emtMarker_de_tgfb = DT::renderDataTable(emtMarkerData[["conditionMDCKTGFb"]]$dataTable)
+  output$emtMarker_de_shZ = DT::renderDataTable(emtMarkerData[["conditionMDCKshZ"]]$dataTable)
 })
--- shiny/MDCK_EMT_RNA-Seq/ui.R
+++ shiny/MDCK_EMT_RNA-Seq/ui.R
@@ -11,12 +11,20 @@ library(shiny)
 library(markdown)
 
 # Define UI for application that draws a histogram
-fluidPage(
-  
-  # Application title
-  titlePanel("MDCK RNA-Seq analysis - Version 0.1"),
-  
+shinyUI(
   fluidPage(
-    includeHTML("mdck_rna-seq.html")
+    
+    # Application title
+    titlePanel("MDCK RNA-Seq analysis - Version 0.1"),
+
+    mainPanel(
+      tabsetPanel(type = "tabs",
+        tabPanel("Volcano plot - EMT markers - TGFb vs WT", plotOutput("volcano_tgfb")),
+        tabPanel("EMT Markers - TGFb vs WT", DT::dataTableOutput("emtMarker_de_tgfb")),
+        tabPanel("Volcano plot - EMT markers - shZ KD vs WT", plotOutput("volcano_shZ")),
+        tabPanel("EMT Markers - shZ KD vs WT", DT::dataTableOutput("emtMarker_de_shZ")),
+        tabPanel("Global DE - TGFb vs WT", DT::dataTableOutput("global_de_tgfb")),
+        tabPanel("Global DE - shZ KD vs WT", DT::dataTableOutput("global_de_shZ"))
+    )
   )
-)
+))
