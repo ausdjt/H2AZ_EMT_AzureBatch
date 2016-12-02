9c10050792ec40e521d633c7062c27fbe431e427
Sebastian Kurscheid <genomicsio@gmail.com>
2016-12-02 19:40:45 +1100

    adds interactive volcano plot for EMT signature genes

--- shiny/MDCK_EMT_RNA-Seq/server.R
+++ shiny/MDCK_EMT_RNA-Seq/server.R
@@ -1,20 +1,18 @@
-# This is the server logic of a Shiny web application. You can run the 
-#
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# This is the server logic of a Shiny web application
 
 library(shiny)
 library(DT)
 
-# Data loading and preparation
-load("~/Data/Tremethick/EMT/RNA-Seq/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/processed_data/CanFam3.1_ensembl84_ERCC/R_Analysis/resultsCompressed.rda", .GlobalEnv)
-load("~/Data/Tremethick/EMT/RNA-Seq/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/processed_data/CanFam3.1_ensembl84_ERCC/R_Analysis/cfam.qPCRGenesTab.rda", .GlobalEnv)
-load("~/Data/Tremethick/EMT/RNA-Seq/NB501086_0082_RDomaschenz_JCSMR_mRNAseq/processed_data/CanFam3.1_ensembl84_ERCC/R_Analysis/ensGenes_CanFam3.1_ensembl84_ERCC.rda", .GlobalEnv)
 
+# load external data files ------------------------------------------------
+load("data/resultsCompressed.rda", .GlobalEnv)
+load("data/cfam.qPCRGenesTab.rda", .GlobalEnv)
+load("data/ensGenes_CanFam3.1_ensembl84_ERCC.rda", .GlobalEnv)
+load("data/cfamEnsGenesSigEMTCells.rda", .GlobalEnv)
+
+
+# prepare data sets -----------------------------------------------------
+# global data used for tables of all genes
 globalData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), function(y){
   s <- resultsCompressed[[1]]$sleuth_results.gene[[y]]$target_id %in% ensGenes$ensembl_gene_id
   dat <- resultsCompressed[[1]]$sleuth_results.gene[[y]][s,]
@@ -27,7 +25,8 @@ globalData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), function
 })
 names(globalData) <- names(resultsCompressed[[1]]$sleuth_results.gene)
 
-emtMarkerData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), function(y){
+# this is the data corresponding to the genes present on the EMT qPCR array
+emtqPCRData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), function(y){
   s <- resultsCompressed[[1]]$sleuth_results.gene[[y]]$target_id %in% cfam.qPCRGenesTab$ensembl_gene_id
   dat <- resultsCompressed[[1]]$sleuth_results.gene[[y]][s,]
   dat <- merge(dat,
@@ -37,13 +36,41 @@ emtMarkerData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), funct
   dat <- dat[order(dat$qval), ]
   return(list(dataTable = dat))
 })
-names(emtMarkerData) <- names(resultsCompressed[[1]]$sleuth_results.gene)
+names(emtqPCRData) <- names(resultsCompressed[[1]]$sleuth_results.gene)
+
+# this is the data corresponding to the EMT cell line signature from Tan et al. 2014
+emtSignatureData <- lapply(names(resultsCompressed[[1]]$sleuth_results.gene), function(y){
+  s <- resultsCompressed[[1]]$sleuth_results.gene[[y]]$target_id %in% cfamEnsGenesSigEMTCells$ensembl_gene_id
+  dat <- resultsCompressed[[1]]$sleuth_results.gene[[y]][s,]
+  dat <- merge(dat,
+               ensGenes[,c("ensembl_gene_id", "external_gene_name", "description")], 
+               by.x = "target_id", 
+               by.y = "ensembl_gene_id")
+  dat <- merge(dat, 
+               cfamEnsGenesSigEMTCells[,c("ensembl_gene_id", "epi_mes")],
+               by.x = "target_id",
+               by.y = "ensembl_gene_id")
+  dat <- dat[order(dat$qval), ]
+  dat$log10_qval <- -log10(dat$qval)
+  return(list(dataTable = dat))
+})
+names(emtSignatureData) <- names(resultsCompressed[[1]]$sleuth_results.gene)
+
+#********************End of data preparation*******************************
+
+# Actual Server logic -----------------------------------------------------
+shinyServer(function(input, output, session) {
   
-# Actual Server logic
-shinyServer(function(input, output) {
+  # add an HTML file
+  getPage<-function() {
+    return(includeHTML("mdck_rna-seq.html"))
+  }
+  output$inc_html <- renderUI({getPage()})
+ 
+  # volcano plots
   output$volcano_tgfb <- renderPlot({
     y <- "conditionMDCKTGFb"
-    dat <- emtMarkerData[[y]]$dataTable
+    dat <- emtqPCRData[[y]]$dataTable
     xAxisMax <- max(abs(dat$b)) + 1
     plot(dat$b,
          -log10(dat$qval), 
@@ -83,7 +110,7 @@ shinyServer(function(input, output) {
   
   output$volcano_shZ <- renderPlot({
     y <- "conditionMDCKshZ"
-    dat <- emtMarkerData[[y]]$dataTable
+    dat <- emtqPCRData[[y]]$dataTable
     xAxisMax <- max(abs(dat$b)) + 1
     plot(dat$b,
          -log10(dat$qval), 
@@ -121,8 +148,89 @@ shinyServer(function(input, output) {
     text(-4,1.2, "< 0.1 [adjusted p-value]", cex = 0.7)
   })
   
-  output$global_de_tgfb = DT::renderDataTable(globalData[["conditionMDCKTGFb"]]$dataTable)
-  output$global_de_shZ = DT::renderDataTable(globalData[["conditionMDCKshZ"]]$dataTable)
-  output$emtMarker_de_tgfb = DT::renderDataTable(emtMarkerData[["conditionMDCKTGFb"]]$dataTable)
-  output$emtMarker_de_shZ = DT::renderDataTable(emtMarkerData[["conditionMDCKshZ"]]$dataTable)
-})
+  selCols <- c("target_id", "qval", "b", "external_gene_name", "description")
+  output$global_de_tgfb = DT::renderDataTable(globalData[["conditionMDCKTGFb"]]$dataTable[,selCols])
+  output$global_de_shZ = DT::renderDataTable(globalData[["conditionMDCKshZ"]]$dataTable[,selCols])
+  output$emtMarker_de_tgfb = DT::renderDataTable(emtqPCRData[["conditionMDCKTGFb"]]$dataTable[,selCols])
+  output$emtMarker_de_shZ = DT::renderDataTable(emtqPCRData[["conditionMDCKshZ"]]$dataTable[,selCols])
+  
+  # interactive volcano plot of the EMT signature data
+  highlight <- reactive({
+    switch(input$highlight,
+           epi = "epi",
+           mes = "mes",
+           none = "none")
+  })
+  
+  output$text <- renderText(paste("you have selected", input$dataset))
+  
+  emtSigPlotData <- reactive({
+    switch(input$dataset, 
+           conditionMDCKTGFb = emtSignatureData[["conditionMDCKTGFb"]]$dataTable,
+           conditionMDCKshZ = emtSignatureData[["conditionMDCKshZ"]]$dataTable
+          )
+  })
+  output$plotui <- renderUI({
+    plotOutput("plot", height=600,
+               click = "plot_click",
+               dblclick = dblclickOpts(
+                 id = "plot_dblclick"
+               ),
+               hover = hoverOpts(
+                 id = "plot_hover"
+               ),
+               brush = brushOpts(
+                 id = "plot_brush"
+               )
+    )
+  }) # output$plotui
+  
+  output$plot <- renderPlot({
+    dat <- emtSigPlotData()
+    
+    xAxisMax <- max(abs(dat$b)) + 1
+    plot(dat$b, dat$log10_qval, axes = F, xlab = "", ylab = "", frame = F, 
+         cex = 0.3,xlim = c(-round(xAxisMax, 0), round(xAxisMax,0)), pch = 16, 
+         main = paste(input$dataset))
+    if (input$highlight == "epi") {
+      w1 <- dat$epi_mes == "epi"
+      colPoint <- c("blue", "black")[match(dat[w1,]$epi_mes, c("epi", "mes"))]
+      points(dat[w1, "b"], dat[w1, "log10_qval"], col = colPoint, pch = 16, cex = 1.1)
+    } else if (input$highlight == "mes") {
+      w1 <- dat$epi_mes == "mes"
+      colPoint <- c("black", "green")[match(dat[w1,]$epi_mes, c("epi", "mes"))]
+      points(dat[w1, "b"], dat[w1, "log10_qval"], col = colPoint, pch = 16, cex = 1.1)
+    } else if (input$highlight == "both") {
+      w1 <- grep("epi|mes", dat$epi_mes)
+      colPoint <- c("blue", "green")[match(dat[w1,]$epi_mes, c("epi", "mes"))]
+      points(dat[w1, "b"], dat[w1, "log10_qval"], col = colPoint, pch = 16, cex = 1.1)
+    } else {
+      w1 <- grep("epi|mes", dat$epi_mes);
+      colPoint <- "black"
+      points(dat[w1, "b"], dat[w1, "log10_qval"], pch = 16, cex = 0.4, col = colPoint)
+    }
+    
+    # always highlighting H2AFZ & TGFB1
+    w2 <- grep("H2AFZ|TGFB1", dat$external_gene_name)
+    points(dat[w2, "b"], dat[w2, "log10_qval"], col = "red", pch = 16, cex = 1.3)
+    text(dat[w2, "b"], dat[w2, "log10_qval"], 
+         labels = dat[w2, "external_gene_name" ],
+         cex = 0.8,
+         pos = 4, offset = 0.3)
+    
+    # Axis 
+    axis(2, pos = 0, lwd = 3)
+    axis(1, pos = 0, lwd = 3, at = c(seq(-round(xAxisMax, 0), round(xAxisMax,0), 2)))
+    mtext("-log10(q-value)", side = 2)
+    mtext("beta value", side = 1, line = 2)
+    abline(h = 1, col = "red", lty = 2, lwd = 2)
+   }) # output$plot
+  
+  selCols <- c(selCols, "epi_mes")
+  output$plot_brushed_points <- DT::renderDataTable({
+    dat <- emtSigPlotData()
+    res <- brushedPoints(dat, input$plot_brush, "b", "log10_qval")
+    datatable(res[,selCols])
+  })
+  
+ })
--- shiny/MDCK_EMT_RNA-Seq/ui.R
+++ shiny/MDCK_EMT_RNA-Seq/ui.R
@@ -9,13 +9,14 @@
 
 library(shiny)
 library(markdown)
+library(DT)
 
 # Define UI for application that draws a histogram
 shinyUI(
   fluidPage(
     
     # Application title
-    titlePanel("MDCK RNA-Seq analysis - Version 0.1"),
+    titlePanel("MDCK RNA-Seq analysis - Version 0.2"),
 
     mainPanel(
       tabsetPanel(type = "tabs",
@@ -24,7 +25,27 @@ shinyUI(
         tabPanel("Volcano plot - EMT markers - shZ KD vs WT", plotOutput("volcano_shZ")),
         tabPanel("EMT Markers - shZ KD vs WT", DT::dataTableOutput("emtMarker_de_shZ")),
         tabPanel("Global DE - TGFb vs WT", DT::dataTableOutput("global_de_tgfb")),
-        tabPanel("Global DE - shZ KD vs WT", DT::dataTableOutput("global_de_shZ"))
+        tabPanel("Global DE - shZ KD vs WT", DT::dataTableOutput("global_de_shZ")),
+        tabPanel("Quality Control report", htmlOutput("inc_html")),
+        tabPanel("Interactive volcano plot - EMT signature genes", 
+                 fluidRow(
+                    column(width=3,
+                           div(class = "option-group",
+                               radioButtons("dataset", "Data set",
+                                            choices = c("conditionMDCKshZ", "conditionMDCKTGFb"), inline = FALSE),
+                               selectInput("highlight", "Gene group to highlight",
+                                          c("none" = "none",
+                                            "Epithelial markers" = "epi",
+                                            "Mesenchymal markers" = "mes",
+                                            "Both" = "both"),
+                                          selected = "none",
+                                          selectize = FALSE)
+                              )),
+                    column(width = 9, uiOutput("plotui"))
+                    ), #fluidRow 1
+                 fluidRow(DT::dataTableOutput("plot_brushed_points"))
+        )
+      )
     )
   )
-))
+)
