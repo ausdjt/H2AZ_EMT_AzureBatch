00b6ae4c692e57144323b89d6004f1103bc1fc61
Sebastian Kurscheid <genomicsio@gmail.com>
2016-12-06 09:40:32 +1100

    bug fix

--- snakemake/rules/run_bowtie2.py
+++ snakemake/rules/run_bowtie2.py
@@ -26,14 +26,13 @@ rule bowtie2_pe:
         bt2_index=config["references"]["CanFam3.1"]["genome"]
     input:
         read1="{assayID}/{runID}/{outdir}/trimmed_data/{unit}_R1_001.QT.CA.fastq.gz",
-        read2="{assayID}/{runID}/{outdir}/trimmed_data/{unit}_R2_001.QT.CA.fastq.gz",
-        bt2_index= home + config["references"]["CanFam3.1"]["genome"]
+        read2="{assayID}/{runID}/{outdir}/trimmed_data/{unit}_R2_001.QT.CA.fastq.gz"
     output:
         protected("{assayID}/{runID}/{outdir}/{reference_version}/{unit}.bam")
     shell:
         """
             bowtie2 \
-            -x {input.bt2_index}\
+            -x {params.bt2_index}\
             --no-mixed \
             --no-discordant \
             --maxins {params.max_in} \
@@ -126,16 +125,16 @@ rule bowtie2_pe_unmapped_reads:
         "Running second round of bowtie2 alignments on previously unmapped reads..."
     params:
         threads = config["program_parameters"]["bt2_params"]["threads"],
-        max_in = config["program_parameters"]["bt2_params"]["max_insert"]
-    input:
-        rules.cutadapt_pe_unmapped.output,
+        max_in = config["program_parameters"]["bt2_params"]["max_insert"],
         bt2_index=config["references"]["CanFam3.1"]["genome"]
+    input:
+        rules.cutadapt_pe_unmapped.output
     output:
         protected("{assayID}/{runID}/{outdir}/{reference_version}/second_alignment/{unit}.2ndAlignment.bam")
     shell:
         """
             bowtie2 \
-            -x {input.bt2_index}\
+            -x {params.bt2_index}\
             --maxins {params.max_in} \
             --threads {params.threads}\
             --very-sensitive\
