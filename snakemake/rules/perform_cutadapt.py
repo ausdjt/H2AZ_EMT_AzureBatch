fa26467c752490067377e5c58e5850c376c6debe
Sebastian Kurscheid <genomicsio@gmail.com>
2016-12-05 17:39:44 +1100

    fixes missing target between cutadapt and bowtie2

--- snakemake/rules/perform_cutadapt.py
+++ snakemake/rules/perform_cutadapt.py
@@ -26,8 +26,8 @@ rule cutadapt_pe:
         read1 = lambda wildcards: wildcards.assayID + "/" + wildcards.runID + "/fastq/" + config["samples"][wildcards.assayID][wildcards.runID][wildcards.unit][0],
         read2 = lambda wildcards: wildcards.assayID + "/" + wildcards.runID + "/fastq/" + config["samples"][wildcards.assayID][wildcards.runID][wildcards.unit][1]
     output:
-        trimmed_read1 = "{assayID}/{runID}/{processed_dir}/{trim_data}/{unit}_R1_001.QT.CA.fastq.gz",
-        trimmed_read2 = "{assayID}/{runID}/{processed_dir}/{trim_data}/{unit}_R2_001.QT.CA.fastq.gz"
+        trimmed_read1 = "{assayID}/{runID}/{outdir}/trimmed_data/{unit}_R1_001.QT.CA.fastq.gz",
+        trimmed_read2 = "{assayID}/{runID}/{outdir}/trimmed_data/{unit}_R2_001.QT.CA.fastq.gz"
     shell:
         """
             {params.cutadapt_dir}/cutadapt {params.trim_params} \
