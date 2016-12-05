877e2c951701a1b6196561d9e630728b353eb429
Sebastian Kurscheid <genomicsio@gmail.com>
2016-12-05 17:21:10 +1100

    adds rules for re-processing ChIP-Seq data

---
+++ snakemake/rules/bam_processing.py
@@ -0,0 +1,82 @@
+__author__ = "Sebastian Kurscheid (sebastian.kurscheid@anu.edu.au)"
+__license__ = "MIT"
+__date__ = "2016-02-29"
+
+# vim: syntax=python tabstop=4 expandtab
+# coding: utf-8
+
+
+"""
+Rules for processing SAM/BAM files
+For usage, include this in your workflow.
+"""
+
+# import other packages
+import os
+import fnmatch
+from snakemake.exceptions import MissingInputException
+
+# set some local variables
+home = os.environ['HOME']
+
+rule:
+    version: 0.1
+
+# rules
+rule bam_quality_filter:
+    params:
+        qual = config["alignment_quality"]
+    input:
+        rules.bowtie2_pe.output
+    output:
+        temp("{assayID}/{runID}/{outdir}/{reference_version}/quality_filtered/{unit}.Q{qual}.bam")
+    shell:
+        "samtools view -b -h -q {params.qual} {input} > {output}"
+
+rule bam_sort:
+    params:
+        qual = config["alignment_quality"]
+    input:
+        rules.bam_quality_filter.output
+    output:
+        temp("{assayID}/{runID}/{outdir}/{reference_version}/sorted/{unit}.Q{qual}.sorted.bam")
+    shell:
+        "samtools sort {input} -T {wildcards.unit}.Q{params.qual}.sorted -o {output}"
+
+rule bam_mark_duplicates:
+    params:
+        qual = config["alignment_quality"],
+        picard = home + config["picard"],
+        temp = home + config["temp_data"]
+    input:
+        rules.bam_sort.output
+    output:
+        protected("{assayID}/{runID}/{outdir}/{reference_version}/duplicates_marked/{unit}.Q{qual}.sorted.MkDup.bam")
+    shell:
+        """
+            java -Djava.io.tmpdir={params.temp} \
+            -Xmx36G \
+            -jar {params.picard} MarkDuplicates \
+            INPUT={input}\
+            OUTPUT={output}\
+            ASSUME_SORTED=TRUE\
+            METRICS_FILE={output}.metrics.txt
+        """
+
+rule bam_index:
+    params:
+        qual = config["alignment_quality"]
+    input:
+        rules.bam_mark_duplicates.output
+    output:
+        protected("{assayID}/{runID}/{outdir}/{reference_version}/duplicates_marked/{unit}.Q{qual}.sorted.MkDup.bam.bai")
+    shell:
+        "samtools index {input} {output}"
+
+rule bam_rmdup:
+    input:
+        rules.bam_mark_duplicates.output
+    output:
+        protected("{assayID}/{runID}/{outdir}/{reference_version}/duplicates_removed/{unit}.Q{qual}.sorted.DeDup.{suffix}")
+    shell:
+        "samtools rmdup {input} {output[0]}; samtools index {output[0]} {output[1]}"
---
+++ snakemake/workflows/MDCK_ChIP-Seq.py
@@ -0,0 +1,56 @@
+_author__ = "Sebastian Kurscheid (sebastian.kurscheid@anu.edu.au)"
+__license__ = "MIT"
+__date__ = "2016-12-05"
+
+from snakemake.exceptions import MissingInputException
+import os
+
+rule:
+    version: 0.1
+
+localrules:
+    all, run_kallisto, run_STAR, run_htseq, run_cutadapt
+
+home = os.environ['HOME']
+
+wrapper_dir = home + "/Development/snakemake-wrappers/bio"
+
+include_prefix = home + "/Development/JCSMR-Tremethick-Lab/H2AZ_EMT/snakemake/rules/"
+
+# define parameters used in this workflow for config lookup
+assayID = "ChIP-Seq""
+runID = "NB501086_0011_MNekrasov_MDCK_JCSMR_ChIPseq"
+
+# include:
+include:
+    include_prefix + "perform_cutadapt.py"
+include:
+    include_prefix + "run_bowtie2.py"
+include:
+    include_prefix + "bam_processing.py"
+
+rule run_cutadapt:
+    input:
+        expand("{assayID}/{runID}/{outdir}/{trim_data}/{unit}_{suffix}.QT.CA.fastq.gz",
+               assayID = assayID,
+               runID = runID,
+               outdir = config["processed_dir"],
+               trim_data = config["trim_dir"],
+               unit = config["samples"][assayID][runID],
+               suffix = ["R1_001", "R2_001"])
+
+rule all:
+    input:
+        expand("{assayID}/{runID}/{outdir}/{reference_version}/duplicates_marked/{unit}.Q{qual}.sorted.MkDup.bam.bai",
+               assayID = assayID,
+               runID = runID,
+               outdir = config["processed_dir"],
+               reference_version = config["references"]["CanFam3.1"]["version"][0],
+               unit = config["samples"][assayID][runID])
+        expand("{assayID}/{runID}/{outdir}/{reference_version}/duplicates_removed/{unit}.Q{qual}.sorted.DeDup.{suffix}",
+               assayID = assayID,
+               runID = runID,
+               outdir = config["processed_dir"],
+               reference_version = config["references"]["CanFam3.1"]["version"][0],
+               unit = config["samples"][assayID][runID],
+               suffix = ["bam", "bam.bai"])
