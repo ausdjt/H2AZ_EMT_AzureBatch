6aebc4e8a3d61926b34865f5abf12c819eb23ec5
Sebastian Kurscheid <genomicsio@gmail.com>
2016-12-08 14:43:10 +1100

    fixes samtools sort command (hoepfully)

--- snakemake/rules/bam_processing.py
+++ snakemake/rules/bam_processing.py
@@ -42,7 +42,7 @@ rule bam_sort:
     output:
         "{assayID}/{runID}/{outdir}/{reference_version}/bowtie2/sorted/{unit}.Q{qual}.sorted.bam"
     shell:
-        "samtools sort {input} -@ {params.threads} -o > {output}"
+        "samtools sort -@ {params.threads} -f {input} {output}"
 
 rule bam_mark_duplicates:
     params:
