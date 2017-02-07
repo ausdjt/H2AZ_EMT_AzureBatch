5c79d0ea83bd3daa50ab011ce7496b85ab8d3878
Sebastian Kurscheid <genomicsio@gmail.com>
2017-02-07 12:48:50 +1100

    see previous

--- snakemake/rules/pooled_replicates_processing.py
+++ snakemake/rules/pooled_replicates_processing.py
@@ -14,43 +14,43 @@ def cli_parameters_computeMatrix(wildcards):
         a["--referencePoint"] = wildcards.referencePoint
     return(a)
 
-# rule run_computeMatrix_pooled_replicates:
-#     input:
-#         expand("{assayID}/{runID}/{outdir}/{reference_version}/{application}/computeMatrix/{command}/{duplicates}/{referencePoint}/{sample_group}_{region}_{mode}.matrix.gz",
-#                assayID = ASSAYID,
-#                runID = RUNID,
-#                outdir = OUTDIR,
-#                reference_version = REFVERSION,
-#                application = "deepTools",
-#                tool = "computeMatrix",
-#                command = ["reference-point", "scale-regions"],
-#                duplicates = ["duplicates_marked", "duplicates_removed"],
-#                referencePoint = "TSS",
-#                sampleGroup = ["H2AZ-WT", "H2AZ-TGFb", "Input-WT", "Input-TGFb"],
-#                region = ["allGenes", "Tan_EMT_up", "Tan_EMT_down"],
-#                mode = ["MNase", "normal"])
+rule run_computeMatrix_pooled_replicates:
+    input:
+        expand("{assayID}/{runID}/{outdir}/{reference_version}/{application}/computeMatrix/{command}/{duplicates}/{referencePoint}/{sample_group}_{region}_{mode}.matrix.gz",
+               assayID = ASSAYID,
+               runID = RUNID,
+               outdir = OUTDIR,
+               reference_version = REFVERSION,
+               application = "deepTools",
+               tool = "computeMatrix",
+               command = ["reference-point", "scale-regions"],
+               duplicates = ["duplicates_marked", "duplicates_removed"],
+               referencePoint = "TSS",
+               sampleGroup = ["H2AZ-WT", "H2AZ-TGFb", "Input-WT", "Input-TGFb"],
+               region = ["allGenes", "Tan_EMT_up", "Tan_EMT_down"],
+               mode = ["MNase", "normal"])
 
-# rule computeMatrix_pooled_replicates:
-#     version:
-#         0.2
-#     params:
-#         deepTools_dir = home + config["deepTools_dir"],
-#         program_parameters = lambda wildcards: ' '.join("{!s}={!s}".format(key, val.strip("\\'")) for (key, val) in cli_parameters_computeMatrix(wildcards).items())
-#     threads:
-#         lambda wildcards: int(str(config["program_parameters"]["deepTools"]["threads"]).strip("['']"))
-#     input:
-#         file = "{assayID}/{runID}/{outdir}/{reference_version}/{application}/bamCoverage/{mode}/{duplicates}/merged_replicates/{sampleGroup}_{mode}_{norm}.bw",
-#         region = lambda wildcards: home + config["program_parameters"]["deepTools"]["regionFiles"][wildcards.region]
-#     output:
-#         matrix_gz = "{assayID}/{runID}/{outdir}/{reference_version}/{application}/computeMatrix/{command}/{duplicates}/{referencePoint}/{sampleGroup}_{region}_{mode}.matrix.gz"
-#     shell:
-#         """
-#             {params.deepTools_dir}/computeMatrix {wildcards.command} \
-#                                                  --regionsFileName {input.region} \
-#                                                  --scoreFileName {input.file} \
-#                                                  --missingDataAsZero \
-#                                                  --skipZeros \
-#                                                  --numberOfProcessors {threads} \
-#                                                  {params.program_parameters} \
-#                                                  --outFileName {output.matrix_gz}
-#         """
+rule computeMatrix_pooled_replicates:
+    version:
+        0.2
+    params:
+        deepTools_dir = home + config["deepTools_dir"],
+        program_parameters = lambda wildcards: ' '.join("{!s}={!s}".format(key, val.strip("\\'")) for (key, val) in cli_parameters_computeMatrix(wildcards).items())
+    threads:
+        lambda wildcards: int(str(config["program_parameters"]["deepTools"]["threads"]).strip("['']"))
+    input:
+        file = "{assayID}/{runID}/{outdir}/{reference_version}/{application}/bamCoverage/{mode}/{duplicates}/merged_replicates/{sampleGroup}_{mode}_{norm}.bw",
+        region = lambda wildcards: home + config["program_parameters"]["deepTools"]["regionFiles"][wildcards.region]
+    output:
+        matrix_gz = "{assayID}/{runID}/{outdir}/{reference_version}/{application}/computeMatrix/{command}/{duplicates}/{referencePoint}/{sampleGroup}_{region}_{mode}.matrix.gz"
+    shell:
+        """
+            {params.deepTools_dir}/computeMatrix {wildcards.command} \
+                                                 --regionsFileName {input.region} \
+                                                 --scoreFileName {input.file} \
+                                                 --missingDataAsZero \
+                                                 --skipZeros \
+                                                 --numberOfProcessors {threads} \
+                                                 {params.program_parameters} \
+                                                 --outFileName {output.matrix_gz}
+        """
