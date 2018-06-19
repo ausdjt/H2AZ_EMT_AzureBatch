c47b22e70ac9d56fb2635c0cad4d589f4ff71cd6
Sebastian Kurscheid <genomicsio@gmail.com>
2018-06-19 14:14:01 +1000

    tidies up code

--- snakemake/workflows/bowtie2_subworkflow.py
+++ snakemake/workflows/bowtie2_subworkflow.py
@@ -20,9 +20,6 @@ include_prefix= home + "/Development/JCSMR-Tremethick-Lab/H2AZ_EMT/snakemake/rul
 include:
     include_prefix + "run_bowtie2.py"
 
-#include:
-#    include_prefix + "bam_processing.py"
-
 # run parameters as variables
 RUNID = "NB501086_0011_MNekrasov_MDCK_JCSMR_ChIPseq"
 ASSAYID = "ChIP-Seq"
